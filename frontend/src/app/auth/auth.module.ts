00a3a043ce5792b980a5ead298c8659e45fd985a
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 23:14:38 +0300

    [ISIS-10] add auth routing

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [AuthPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
