b522ed23049a5d5686e91d0dfae93d859a5369e5
OverldAndrey <andrey3000.99@mail.ru>
2021-04-02 00:19:48 +0300

    [ISIS-10] fix guard errors

--- frontend/src/app/app/app.module.ts
+++ frontend/src/app/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
