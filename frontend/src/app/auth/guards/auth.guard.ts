b334fd68f0a9c9b1edef6bdd62155c4fe7018d19
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 23:26:05 +0300

    [ISIS-10] implement guards

--- frontend/src/app/auth/guards/auth.guard.ts
+++ frontend/src/app/auth/guards/auth.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+    return this.auth.isAuthed ? true : this.router.parseUrl(`/auth/sign-in?redirect=${state.url}`);
   }
-  
 }
