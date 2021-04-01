dfff022d49c97bf791f967ad42b6f6b5877b3143
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 23:56:01 +0300

    [ISIS-10] implement auth component logic

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthPageComponent } from './components/auth-page/auth-page.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,8 @@ import { AuthRoutingModule } from './auth-routing.module';
   declarations: [AuthPageComponent],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
   ]
 })
 export class AuthModule { }
