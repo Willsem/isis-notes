66a194434240b97460e33f7c07f3ec5604ba0d58
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 22:27:15 +0300

    [ISIS-10] create auth module files

---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthPageComponent } from './components/auth-page/auth-page.component';
+
+
+
+@NgModule({
+  declarations: [AuthPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthModule { }
