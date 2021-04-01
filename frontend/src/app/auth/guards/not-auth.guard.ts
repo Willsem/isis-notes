c78d131a23f71a3858b1b93d875fc18a6d9d6e58
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 23:26:05 +0300

    [ISIS-10] implement guards

--- frontend/src/app/auth/guards/not-auth.guard.ts
+++ frontend/src/app/auth/guards/not-auth.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotAuthGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+  ) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+    return this.auth.isAuthed ? this.router.parseUrl('/') : true;
   }
-  
+
 }
