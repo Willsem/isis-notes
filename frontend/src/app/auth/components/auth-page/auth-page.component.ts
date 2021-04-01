d003012a92591935e8a4b3e610bc0f8d88e518ae
OverldAndrey <andrey3000.99@mail.ru>
2021-04-01 23:56:01 +0300

    [ISIS-10] implement auth component logic

--- frontend/src/app/auth/components/auth-page/auth-page.component.ts
+++ frontend/src/app/auth/components/auth-page/auth-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth-page',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent implements OnInit {
+  public username = '';
+  public password = '';
 
-  constructor() { }
+  constructor(
+    public router: Router,
+    public auth: AuthService,
+    public route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  public async onLogin(): Promise<void> {
+    const session = await this.auth.login({
+      username: this.username,
+      password: this.password,
+    });
+
+    if (session) {
+      await this.redirectAfterLogin();
+    }
+  }
+
+  public onClear(): void {
+    this.username = '';
+    this.password = '';
+  }
+
+  public async redirectAfterLogin(): Promise<void> {
+    const redirect = this.route.snapshot.queryParamMap.get('redirect') || '/';
+    await this.router.navigateByUrl(redirect);
+  }
 }
