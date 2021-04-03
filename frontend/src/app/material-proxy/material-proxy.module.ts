a38832c5f83fa697d61899d45d71b44d05bf74ad
Willsem <happymainer@gmail.com>
2021-04-03 14:22:23 +0300

    [ISIS-10] remake design to material

--- frontend/src/app/material-proxy/material-proxy.module.ts
+++ frontend/src/app/material-proxy/material-proxy.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
+import { MatButtonModule } from '@angular/material/button';
 
 const materialModules = [
   MatInputModule,
   MatFormFieldModule,
+  MatButtonModule,
 ];
 
 @NgModule({
