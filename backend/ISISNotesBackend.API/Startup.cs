45160fa6ffdaea81b0271721cd7209e73c20b220
NastyRu <nasty.sidenko@mail.ru>
2021-04-04 16:00:58 +0300

    [ISIS-12] Add settings and startup for migration

--- backend/ISISNotesBackend.API/Startup.cs
+++ backend/ISISNotesBackend.API/Startup.cs
@@ -5,17 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ISISNotesBackend.DataBase.NpgsqlContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace ISISNotesBackend.API
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ISISNotesContext>(options => 
+                options.UseNpgsql(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
