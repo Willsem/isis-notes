ba6c4478fd4deeaed563b76c2d199570b859a2d0
TinyMarcus <ilyasov9999@gmail.com>
2021-04-04 18:06:26 +0300

    [ISIS-17] add text note core model

---
+++ backend/ISISNotesBackend.Core/Models/TextNote.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ISISNotesBackend.Core.Models
+{
+    public class TextNote
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public Note Note { get; set; }
+
+        public TextNote(Guid id, string text, Note note)
+        {
+            string exceptionMessage = "Wrong parameters: \n";
+            
+            if (note == null)
+            {
+                exceptionMessage += $"'{nameof(note)}': Reference to Note can't be null \n";
+            }
+
+            if (exceptionMessage != "Wrong parameters: \n")
+            {
+                throw new ArgumentException(exceptionMessage);
+            }
+            
+            Id = id;
+            Text = text;
+            Note = note;
+        }
+    }
+}
