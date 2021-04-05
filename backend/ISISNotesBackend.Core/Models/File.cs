095fd779f9d54f720958a46bba1bbb03c9a1aebb
TinyMarcus <ilyasov9999@gmail.com>
2021-04-05 09:41:35 +0300

    [ISIS-17] add text note reference in file core model and its validation

--- backend/ISISNotesBackend.Core/Models/File.cs
+++ backend/ISISNotesBackend.Core/Models/File.cs
@@ -7,18 +7,23 @@ namespace ISISNotesBackend.Core.Models
         public Guid Id { get; set; }
         public string FilePath { get; set; }
         public FileType FileType { get; set; }
+        public TextNote TextNote { get; set; }
 
-        public File(Guid id, string filePath, FileType fileType)
+        public File(Guid id, string filePath, FileType fileType, TextNote textNote)
         {
             string exceptionMessage = "Wrong parameters: \n";
             
             if (fileType == null)
             {
-                exceptionMessage += $"'{nameof(fileType)}': Type of file can't be null \n";
+                exceptionMessage += $"'{nameof(fileType)}': Reference to FileType can't be null \n";
             }
             if (string.IsNullOrEmpty(filePath))
             {
                 exceptionMessage += $"'{nameof(filePath)}': Path of file can't be null \n";
+            }
+            if (textNote == null)
+            {
+                exceptionMessage += $"'{nameof(textNote)}': Reference to TextNote can't be null \n";
             }
 
             if (exceptionMessage != "Wrong parameters: \n")
@@ -29,6 +34,7 @@ namespace ISISNotesBackend.Core.Models
             Id = id;
             FilePath = filePath;
             FileType = fileType;
+            TextNote = textNote;
         }
     }
 }
