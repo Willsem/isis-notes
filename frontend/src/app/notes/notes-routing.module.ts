dba02bb862f3eebec75baadcba345c41a6a203c7
OverldAndrey <andrey3000.99@mail.ru>
2021-04-08 16:11:11 +0300

    [ISIS-18] setup routing for notes

--- frontend/src/app/notes/notes-routing.module.ts
+++ frontend/src/app/notes/notes-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotesComponent } from './components/notes/notes.component';
 
 const routes: Routes = [
-
+  { path: '', component: NotesComponent }
 ];
 
 @NgModule({
