282dec70645daf3bf38c909ba1c506aa646cdcfa
OverldAndrey <andrey3000.99@mail.ru>
2021-04-08 16:00:25 +0300

    [ISIS-18] Create notes module

---
+++ frontend/src/app/notes/notes.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { NotesRoutingModule } from './notes-routing.module';
+import { MaterialProxyModule } from '../material-proxy/material-proxy.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    NotesRoutingModule,
+    MaterialProxyModule
+  ]
+})
+export class NotesModule { }
