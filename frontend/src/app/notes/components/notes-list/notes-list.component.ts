4abee1ede079058a21db6000056244e35b14f6fc
OverldAndrey <andrey3000.99@mail.ru>
2021-04-11 18:06:03 +0300

    [ISIS-19] create notes list

---
+++ frontend/src/app/notes/components/notes-list/notes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'isis-notes-list',
+  templateUrl: './notes-list.component.html',
+  styleUrls: ['./notes-list.component.css']
+})
+export class NotesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/notes/notes.module.ts
+++ frontend/src/app/notes/notes.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { NotesRoutingModule } from './notes-routing.module';
 import { MaterialProxyModule } from '../material-proxy/material-proxy.module';
 import { NotesComponent } from './components/notes/notes.component';
+import { NotesListComponent } from './components/notes-list/notes-list.component';
 
 
 
 @NgModule({
   declarations: [
     NotesComponent,
+    NotesListComponent,
   ],
   imports: [
     NotesRoutingModule,
