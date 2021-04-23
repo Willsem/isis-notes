342d7c7b3537ca90f91cc16ad98ebccc4bf60acd
OverldAndrey <andrey3000.99@mail.ru>
2021-04-23 23:04:04 +0300

    [ISIS-22] layout user edit component

--- frontend/src/app/auth/components/user-details/user-details.component.ts
+++ frontend/src/app/auth/components/user-details/user-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../../shared/models/user';
+import { MatDialog } from '@angular/material/dialog';
+import { UserEditComponent } from '../user-edit/user-edit.component';
 
 @Component({
   selector: 'isis-user-details',
@@ -13,10 +15,15 @@ export class UserDetailsComponent implements OnInit {
 
   constructor(
     public auth: AuthService,
+    public dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
     this.user = this.auth.currentSessionValue.user ?? {id: '', username: '', email: '', avatar: ''};
+  }
+
+  public onEditUser(): void {
+    this.dialog.open(UserEditComponent, { disableClose: true });
   }
 
 }
--- frontend/src/app/auth/components/user-edit/user-edit.component.ts
+++ frontend/src/app/auth/components/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../shared/models/user';
+import {AuthService} from '../../services/auth.service';
+import {UserLogin} from '../../../shared/models/user-login';
+import {FormControl} from '@angular/forms';
+import {ApiService} from '../../../api/services/api.service';
+import {MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'isis-user-edit',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  public user: User = this.auth.currentSessionValue.user;
+  public fullUserDataForm = new FormControl({
+    login: this.user.username,
+    email: this.user.email,
+    password: '',
+    confirmPassword: '',
+  });
+
+  public file = this.user.avatar;
+  public fileBinary;
+
+  constructor(
+    public auth: AuthService,
+    public api: ApiService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onAvatarChange(event: Event): void {
+    console.log(event);
+    const file = (event.target as HTMLInputElement).files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = (e) => {
+      this.file = fileReader.result as string;
+      console.log(this.file);
+    };
+    file.arrayBuffer().then(buf => this.fileBinary = new Blob([buf]));
+    fileReader.readAsDataURL(file);
+  }
+
+  onUpdate(): void {
+    console.log('Update');
+  }
 }
