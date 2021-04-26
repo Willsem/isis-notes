d10ad6292cfe1bbc12311de5be5f9e98d8558a44
TinyMarcus <ilyasov9999@gmail.com>
2021-04-26 03:19:47 +0300

    [ISIS-35] add CreateUserNote method implementation in UserNoteRepository

--- backend/DataBase/ISISNotesBackend.DataBase.Repositories/UserNoteRepository.cs
+++ backend/DataBase/ISISNotesBackend.DataBase.Repositories/UserNoteRepository.cs
@@ -1,15 +1,40 @@
 using System;
+using System.Linq;
 using ISISNotesBackend.Core.Models;
 using ISISNotesBackend.Core.Models.Enums;
 using ISISNotesBackend.Core.Repositories;
+using ISISNotesBackend.DataBase.NpgsqlContext;
+using Microsoft.EntityFrameworkCore;
+using DbModels = ISISNotesBackend.DataBase.Models;
+using CoreModels = ISISNotesBackend.Core.Models;
 
 namespace ISISNotesBackend.DataBase.Repositories
 {
     public class UserNoteRepository : IUserNoteRepository
     {
+        private readonly ISISNotesContext _dbContext;
+
+        public UserNoteRepository(ISISNotesContext chatServiceContext)
+        {
+            _dbContext = chatServiceContext;
+        }
+        
         public NoteAccessRight CreateUserNote(Guid changeUserId, Guid userId, Guid noteId, UserRights userRights)
         {
-            throw new NotImplementedException();
+            var user = _dbContext.Users
+                .First(u => u.Id == userId);
+            
+            var note = _dbContext.Notes
+                .First(n => n.Id == noteId);
+            
+            var userNote = new DbModels.UserNote((DbModels.Enums.UserRights) userRights, userId, user, noteId, note);
+
+            _dbContext.UserNotes.Add(userNote);
+            _dbContext.SaveChanges();
+            
+            return new NoteAccessRight(noteId.ToString(), 
+                userId.ToString(),
+                userRights);
         }
 
         public NoteAccessRight ChangeUserNote(Guid changeUserId, Guid userId, Guid noteId, UserRights userRights)
