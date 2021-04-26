ae9a63d3ff214f57683e8df4f0ddef5809ff03da
Willsem <happymainer@gmail.com>
2021-04-26 18:26:03 +0300

    [ISIS-32] added test for note audio file component

--- frontend/src/app/notes/components/note-audio-file/note-audio-file.component.spec.ts
+++ frontend/src/app/notes/components/note-audio-file/note-audio-file.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoteAudioFileComponent } from './note-audio-file.component';
 
 describe('NoteAudioFileComponent', () => {
-  // let component: NoteAudioFileComponent;
-  // let fixture: ComponentFixture<NoteAudioFileComponent>;
-  //
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ NoteAudioFileComponent ]
-  //   })
-  //   .compileComponents();
-  // });
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(NoteAudioFileComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  let component: NoteAudioFileComponent;
+  let fixture: ComponentFixture<NoteAudioFileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoteAudioFileComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoteAudioFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(true).toBeTruthy();
