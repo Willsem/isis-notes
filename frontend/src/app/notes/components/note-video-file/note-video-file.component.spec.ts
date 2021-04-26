059774e6cafdb6d6cc9c04147e04232899d0da2c
Willsem <happymainer@gmail.com>
2021-04-26 18:06:50 +0300

    [ISIS-32] added test for note video file component

--- frontend/src/app/notes/components/note-video-file/note-video-file.component.spec.ts
+++ frontend/src/app/notes/components/note-video-file/note-video-file.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoteVideoFileComponent } from './note-video-file.component';
 
 describe('NoteVideoFileComponent', () => {
-  // let component: NoteVideoFileComponent;
-  // let fixture: ComponentFixture<NoteVideoFileComponent>;
-  //
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ NoteVideoFileComponent ]
-  //   })
-  //   .compileComponents();
-  // });
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(NoteVideoFileComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  let component: NoteVideoFileComponent;
+  let fixture: ComponentFixture<NoteVideoFileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoteVideoFileComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoteVideoFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(true).toBeTruthy();
