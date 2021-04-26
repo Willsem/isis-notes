b035b1e13ae591e3cc0f9b8ae1747c1e5cc8af2d
Willsem <happymainer@gmail.com>
2021-04-26 18:24:36 +0300

    [ISIS-32] added test for note text image component

--- frontend/src/app/notes/components/note-image-file/note-image-file.component.spec.ts
+++ frontend/src/app/notes/components/note-image-file/note-image-file.component.spec.ts
@@ -1,23 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NoteImageFileComponent } from './note-image-file.component';
+import { NoteFilesService } from '../../services/note-files.service';
+
+class NoteFilesServiceMock {
+  public async getFileData(fileId: string): Promise<Blob> {
+    return new Blob;
+  }
+}
 
 describe('NoteImageFileComponent', () => {
-  // let component: NoteImageFileComponent;
-  // let fixture: ComponentFixture<NoteImageFileComponent>;
-  //
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ NoteImageFileComponent ]
-  //   })
-  //   .compileComponents();
-  // });
-  //
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(NoteImageFileComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  let component: NoteImageFileComponent;
+  let fixture: ComponentFixture<NoteImageFileComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoteImageFileComponent ],
+      providers: [
+        { provide: NoteFilesService, useClass: NoteFilesServiceMock }
+      ],
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NoteImageFileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create', () => {
     expect(true).toBeTruthy();
